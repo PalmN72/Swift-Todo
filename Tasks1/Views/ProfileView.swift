5c066fdba7034de222028aa6fd1cf253d8903a93
Palmn72 <cs.palmqvist@gmail.com>
2023-11-15 15:28:34 +0100

    profile page

--- Tasks1/Views/ProfileView.swift
+++ Tasks1/Views/ProfileView.swift
@@ -14,20 +14,52 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
-                
+                if let user = viewModel.user {
+                    
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(Color.blue)
+                        .frame(width: 125, height: 125)
+                        .padding()
+                    
+                    VStack(alignment: .leading) {
+                        
+                        HStack {
+                            Text("Name: ")
+                                .bold()
+                            Text(user.name)
+                        }
+                        .padding(.bottom, 5)
+                        
+                        HStack {
+                            Text("Email: ")
+                                .bold()
+                            Text(user.email)
+                        }
+                        .padding(.bottom, 5)
+                        
+                        HStack {
+                            Text("Member since: ")
+                                .bold()
+                            Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+                        }
+                    }
+                    .padding()
+                    
+                    Button("Log out") {
+                        viewModel.logOut()
+                    }
+                    .tint(Color.red)
+                    .padding()
+                } else {
+                    Text("Loading Profile...")
+                }
             }
-            .navigationTitle(
-                "Profile"
-            )
-            .toolbar {
-                Button(action: {
-                },
-                       label: {
-                    Image(
-                        systemName: "plus"
-                    )
-                })
-            }
+            .navigationTitle("Profile")
+        }
+        .onAppear {
+            viewModel.fetchUser()
         }
     }
 }
