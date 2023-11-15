16e0e64524e097b459ca197ab3a8dd8f63d031fd
Palmn72 <cs.palmqvist@gmail.com>
2023-11-15 14:40:19 +0100

    Delete item from firebase

--- Tasks1/ViewModels/ToDoListViewViewModel.swift
+++ Tasks1/ViewModels/ToDoListViewViewModel.swift
@@ -5,14 +5,26 @@
 //  Created by Salt Dev on 2023-11-14.
 //
 
+import FirebaseFirestore
 import Foundation
 
 class ToDoListViewViewModel: ObservableObject {
     
     @Published var showingNewItemView = false
-    init() {}
+    
+    private let userId: String
+    
+    init(userId: String) {
+        self.userId = userId
+    }
+    
     
     func delete(id: String) {
-        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(userId)
+            .collection("todos")
+            .document(id)
+            .delete()
     }
 }
