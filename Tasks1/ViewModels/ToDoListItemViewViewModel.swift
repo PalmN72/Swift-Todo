88fd61609cc37804ee5c407feae6f600bc4bf902
Palmn72 <cs.palmqvist@gmail.com>
2023-11-15 15:06:23 +0100

    fix: implemented item manipulation into correct file

--- Tasks1/ViewModels/ToDoListItemViewViewModel.swift
+++ Tasks1/ViewModels/ToDoListItemViewViewModel.swift
@@ -5,9 +5,28 @@
 //  Created by Salt Dev on 2023-11-14.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 /// View model for single item in todolist
 class ToDoListItemViewViewModel: ObservableObject {
     init() {}
+    
+    func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+        
+    }
     
 }
