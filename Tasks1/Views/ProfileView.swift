c9e2970de0d0a2c9223d713a90a5650b740b1cd2
Palmn72 <cs.palmqvist@gmail.com>
2023-11-15 15:35:13 +0100

    break out stack into viewbuilder

--- Tasks1/Views/ProfileView.swift
+++ Tasks1/Views/ProfileView.swift
@@ -15,43 +15,7 @@ struct ProfileView: View {
         NavigationView {
             VStack {
                 if let user = viewModel.user {
-                    
-                    Image(systemName: "person.circle")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundColor(Color.blue)
-                        .frame(width: 125, height: 125)
-                        .padding()
-                    
-                    VStack(alignment: .leading) {
-                        
-                        HStack {
-                            Text("Name: ")
-                                .bold()
-                            Text(user.name)
-                        }
-                        .padding(.bottom, 5)
-                        
-                        HStack {
-                            Text("Email: ")
-                                .bold()
-                            Text(user.email)
-                        }
-                        .padding(.bottom, 5)
-                        
-                        HStack {
-                            Text("Member since: ")
-                                .bold()
-                            Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
-                        }
-                    }
-                    .padding()
-                    
-                    Button("Log out") {
-                        viewModel.logOut()
-                    }
-                    .tint(Color.red)
-                    .padding()
+                    profile(user: user)
                 } else {
                     Text("Loading Profile...")
                 }
@@ -61,6 +25,46 @@ struct ProfileView: View {
         .onAppear {
             viewModel.fetchUser()
         }
+    }
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(Color.blue)
+            .frame(width: 125, height: 125)
+            .padding()
+        
+        VStack(alignment: .leading) {
+            
+            HStack {
+                Text("Name: ")
+                    .bold()
+                Text(user.name)
+            }
+            .padding(.bottom, 5)
+            
+            HStack {
+                Text("Email: ")
+                    .bold()
+                Text(user.email)
+            }
+            .padding(.bottom, 5)
+            
+            HStack {
+                Text("Member since: ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+        }
+        .padding()
+        
+        Button("Log out") {
+            viewModel.logOut()
+        }
+        .tint(Color.red)
+        .padding()
     }
 }
 
